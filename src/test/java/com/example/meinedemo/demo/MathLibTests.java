cc061d833cefb9b357a1680fda9eb91f5446824c
movik21 <moorvik1@students.zhaw.ch>
2024-03-25 16:29:15 +0100

    Added first test cases to JUnit HelloWorld

--- src/test/java/com/example/meinedemo/demo/MathLibTests.java
+++ src/test/java/com/example/meinedemo/demo/MathLibTests.java
@@ -1,10 +1,18 @@
 package com.example.meinedemo.demo;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class MathLibTests {
-    @Test
-    void testIsEven() {
 
+    @Test
+    void testIsEven1() {
+        assertTrue(MathLib.isEven(2));
+    }
+    @Test
+    void testIsEven2() {
+        assertFalse(MathLib.isEven(3));
     }
 }
+
