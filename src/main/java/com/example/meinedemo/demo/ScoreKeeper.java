79c9a93a7a819c7751a685267df34614f5fc62cc
movik21 <moorvik1@students.zhaw.ch>
2024-03-25 17:30:20 +0100

    Initiale version ScoreKeeper

---
+++ src/main/java/com/example/meinedemo/demo/ScoreKeeper.java
@@ -0,0 +1,8 @@
+package com.example.meinedemo.demo;
+
+public class ScoreKeeper {
+    //TBD void scoreTeamA()
+    //TBD void scoreTeamB()
+    //TBD String getScore()
+    
+}
---
+++ src/test/java/com/example/meinedemo/demo/ScoreKeeperTests.java
@@ -0,0 +1,9 @@
+package com.example.meinedemo.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class ScoreKeeperTests {
+    @Test
+    void test1() {
+    }
+}
