216954643ab14d4a27a3b03911fe01021018e322
movik21 <moorvik1@students.zhaw.ch>
2024-03-26 18:59:29 +0100

    Schritt 9

--- src/test/java/com/example/meinedemo/demo/ScoreKeeperTests.java
+++ src/test/java/com/example/meinedemo/demo/ScoreKeeperTests.java
@@ -24,11 +24,19 @@ public class ScoreKeeperTests {
         assertEquals("000:001", scoreKeeper.getScore());
     }
     @Test
-    public void test4() { // Max points per end
+    public void test4() { // Max points per end - Team A
         ScoreKeeper scoreKeeper = new ScoreKeeper();
         scoreKeeper.scoreTeamA(7); // Wrong input will be corrected automatically
         scoreKeeper.scoreTeamB(2);
         assertEquals("006:002", scoreKeeper.getScore()); 
+    }
+
+    @Test
+    public void test5() { // Max points per end - Team B
+        ScoreKeeper scoreKeeper = new ScoreKeeper();
+        scoreKeeper.scoreTeamA(3); 
+        scoreKeeper.scoreTeamB(100); // Wrong input will be corrected automatically
+        assertEquals("003:006", scoreKeeper.getScore()); 
     }
 
 }
