03d68e3a25e640e59781a1206e88d86dd2c4cd36
movik21 <moorvik1@students.zhaw.ch>
2024-03-26 17:54:03 +0100

    Schritt 6

--- src/main/java/com/example/meinedemo/demo/ScoreKeeper.java
+++ src/main/java/com/example/meinedemo/demo/ScoreKeeper.java
@@ -2,16 +2,19 @@ package com.example.meinedemo.demo;
 
 public class ScoreKeeper {
     
-    private int scoreTeamA;
+    private int scoreTeamA = 0;
+    private int scoreTeamB = 0;
 
     public void scoreTeamA(int points) {
         this.scoreTeamA += points;
     }
     
-    //TBD void scoreTeamB()
+    public void scoreTeamB(int points) {
+        this.scoreTeamB += points;
+    }
 
     public String getScore() {
-        return String.format("%03d:000", scoreTeamA);
+        return String.format("%03d:%03d", scoreTeamA, scoreTeamB);
     }
     
 }
